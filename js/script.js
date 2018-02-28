1bbb8523e21eac5f3a0041e0cf501fe19989d59b
JP Fischer <fischer.johnp@gmail.com>
2018-02-27 21:14:54 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Script to make the dropdown work
+
+function dropDown() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// Script to make the button reveal text in recap
+
+function showMore(){
+
+}
